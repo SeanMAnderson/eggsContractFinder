b55bfef77a934a6d4ed27ce424402fe221188ca8
Sean M <sean.ml.anderson@gmail.com>
2025-01-02 16:50:34 -0600

    first commit

---
+++ eggsMain.py
@@ -0,0 +1,20 @@
+#eggs inc contract finder
+#project goals
+# givben a list of URLS (from discord)
+# parse out the URLS and display a list of contracts with open player spaces
+
+
+#ultimate goal, select contract
+#1. parse URLS into a list
+#2. access API
+#3. display results
+#4. save results
+#     allow addition
+#     allow changing contract
+
+##################
+#step 1
+# make API call the Aux brain??
+# https://github.com/fanaticscripter/Egg EICOOP website
+# https://github.com/derekantrican/EggIncAPI/tree/master/python                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   n,m
+
